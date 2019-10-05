f8add834ff901a6b1c4c2449d2709fe47920958d
Jeff Kloos <jeffrey.kloos@gmail.com>
2019-10-05 18:15:12 +0200

    Created an input system

---
+++ Ludum Dare 45/Assets/ConsoleListItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class ConsoleListItem : MonoBehaviour
+{
+    public TextMeshProUGUI bodyObject;
+
+    private void Start()
+    {
+        bodyObject = GetComponent<TextMeshProUGUI>();
+    }
+
+    public ConsoleListItem InitItem(string text){
+        bodyObject.SetText(text);
+        return this;
+    }
+}
--- Ludum Dare 45/Assets/Scripts/TextAdventureConsole.cs
+++ Ludum Dare 45/Assets/Scripts/TextAdventureConsole.cs
@@ -1,18 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class TextAdventureConsole : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Objects")]
+    public TMP_InputField inputField;
+    public Transform consoleParent;
+
+    [Header("Prefabs")]
+    public GameObject consoleLitItemPrefab;
+
+
+    public string[] History { get { return history.ToArray(); } }
+    private List<string> history;
+
+    private List<ConsoleListItem> consoleListItems;
+
+    private void Start()
     {
-        
+        Focus();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        Init();
     }
+
+    private void Init()
+    {
+        history = new List<string>();
+        consoleListItems = new List<ConsoleListItem>();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            InputLine("> " + inputField.text);
+        }
+    }
+
+    private void InputLine(string input)
+    {
+        WriteLine(input);
+        ParseLine(input);
+        inputField.text = "";
+        Focus();
+    }
+
+    private void ParseLine(string input)
+    {
+        WriteLine("Nice Input!");
+    }
+
+    public void WriteLine(string newLine)
+    {
+        history.Add(newLine);
+        var go = Instantiate(consoleLitItemPrefab, consoleParent).GetComponent<ConsoleListItem>().InitItem(newLine);
+        consoleListItems.Add(go);
+
+    }
+
+    private void Focus()
+    {
+        inputField.text = "";
+        inputField.Select();
+        inputField.ActivateInputField();
+    }
+
+
 }
