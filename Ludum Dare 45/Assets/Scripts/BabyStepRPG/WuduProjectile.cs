8e447f4a3dee5041a14f85dc9494bcc8de83367e
Unknown <rommedegraauw@gmail.com>
2019-10-06 17:13:32 +0200

    Incomplete projectile stuff

---
+++ Ludum Dare 45/Assets/Scripts/BabyStepRPG/WuduProjectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WuduProjectile : MonoBehaviour
+{
+    public float speed = 50.0f;
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.magnitude > 50.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rb.AddForce(direction * force);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+}
